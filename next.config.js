8fe7f72820ad6cb560ad296e7620e0d210277e45
DIBIA AI <dibia.aiagency@gmail.com>
2025-10-22 19:23:50 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  reactStrictMode: true,
-  // Removed invalid 'allowedDevOrigins' - not needed in Next.js 15
-  experimental: {
-    // Add valid experimental features here if needed
-  }
-};
